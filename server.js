17906a617ae14264e8d07f9b44b96cb2b293dcd4
Bão Bão <ducbaonguyen101098@gmail.com>
2021-12-02 16:56:37 +0700

    up

--- server.js
+++ server.js
@@ -1,48 +1,13 @@
-const express = require("express");
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express"); 
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const app = express();
-const httpServer = createServer(app);
-const socket = new Server(httpServer, {
-    cors: {
-        origin: "*",
-    }
-});
+const httpServer = createServer(app); 
 
 
-httpServer.listen(3130);
-const rootRouter = require("./routers/rootRouter");
-const { checkTimeUpdatedAt, updatePairToken, getListPairToken, getAllPairByStatus } = require("./utils/collections/2021_list_pair_token");
+httpServer.listen(process.env.PORT || 3130);
 
 
-const soketFindNewPair = require("./soketFindNewPair"); 
-require("./client/myClient");
-
-let isCheck = false;
-
-global.historyTxt = [];
-
-socket.on("connection", async (client) => {
-    const result = await getListPairToken();
-    const data = result.status === "success" ? result.data : [];
-    if (!isCheck) {
-        soketFindNewPair(socket, data); 
-        isCheck = true;
-    }
-    socket.to(`${client.id}`).emit("getListPairToken", data);
-});
-
-app.set('trust proxy', false);
-app.use(cors());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(rootRouter);
-
-
-
+require("./updatePairToken_1.js")
 
 
